848f1806bf32f319694b88cc8ebf07006f6a11e8
RahulGupta <rahulrgd21@gmail.com>
2024-07-05 14:03:28 +0530

    Added UserService class

--- user-service.demo/src/main/java/com/user_service/demo/service/UserService.java
+++ user-service.demo/src/main/java/com/user_service/demo/service/UserService.java
@@ -1,5 +1,30 @@
 package com.user_service.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.user_service.demo.model.User;
+import com.user_service.demo.repository.UserRepository;
+
+import java.util.List;
+
+@Service
 public class UserService {
-    
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUser(Integer id){
+        try{
+            return userRepository.findById(id).get();
+        }catch (Exception e){
+            return new User();
+        }
+
+    }
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+    public User addUser(User user){
+        return userRepository.save(user);
+    }
 }
