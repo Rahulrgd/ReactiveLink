5625c5bac1c9132ddfebaaeda00b1b9eba53a3a5
RahulGupta <rahulrgd21@gmail.com>
2024-07-05 14:30:52 +0530

    Created addUser method in UserService class

--- webClient.demo/src/main/java/com/webClient/demo/service/UserService.java
+++ webClient.demo/src/main/java/com/webClient/demo/service/UserService.java
@@ -2,14 +2,12 @@ package com.webClient.demo.service;
 
 import com.webClient.demo.config.UserProps;
 import com.webClient.demo.model.User;
-
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.Arrays;
 import java.util.List;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
@@ -34,12 +32,24 @@ public class UserService {
     return user;
   }
 
-  public List<User> getAllUser(){
-        User[] users = webClient.get()
-                .uri("/")
-                .retrieve()
-                .bodyToMono(User[].class)
-                .block();
-        return Arrays.stream(users).toList();
-    }
+  public List<User> getAllUser() {
+    User[] users = webClient
+      .get()
+      .uri("/")
+      .retrieve()
+      .bodyToMono(User[].class)
+      .block();
+    return Arrays.stream(users).toList();
+  }
+
+  public User addUser(User user) {
+    User responseUser = webClient
+      .post()
+      .uri("/")
+      .body(Mono.just(user), User.class)
+      .retrieve()
+      .bodyToMono(User.class)
+      .block();
+    return responseUser;
+  }
 }
